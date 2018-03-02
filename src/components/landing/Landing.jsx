bb7f372b64b84b8948d217484847a8686ed70080
Corey Redding <coreysnightout@gmail.com>
2018-03-01 20:25:39 -0800

    starter files

---
+++ src/components/landing/Landing.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Landing.css';
+import Nav from '../nav/Nav';
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <div className="landing-wrapper">
+        <Nav />
+      </div>
+    )
+  };
+}
+
+export default Landing;
+
+
+// <Hero />
+// <Intro />
+// <Activities />
+// <Maps />
+// <Footer />
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.css';
+import Landing from '../landing/Landing';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <Landing />
+      </div>
+    )
+  };
+}
+
+export default Main;
