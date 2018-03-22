f356a9b1db6b5f4515cd60735489a075522a8323
Corey Redding <coreysnightout@gmail.com>
2018-03-21 21:45:48 -0700

    working on landing and navbar stuff

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Landing.css';
 import Nav from '../nav/Nav';
+import SplashImg from '../../img/splash.jpg';
+import MainSplashLogo from '../../img/main-splash-logo.png';
 
 class Landing extends React.Component {
   render() {
     return (
       <div className="landing-wrapper">
         <Nav />
+        <div className="splash">
+          <img className="splash-img" src={SplashImg} />
+          <img className="main-splash-logo" src={MainSplashLogo} />
+        </div>
       </div>
     )
   };
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import './Nav.css';
 import NavLogo from '../../img/footer-logo.png';
 
+
 class Nav extends React.Component {
   render() {
     return (
-      <div className="nav-bar">
-        <img class="nav-logo" src={NavLogo} alt="logo" />
-        <p>Destinations</p>
-        <p>Trips</p>
-        <p>About</p>
-        <p>Contact</p>
+      <div className="nav-bar transparent">
+        <img className="nav-logo" src={NavLogo} alt="logo" />
+        <div className="links">
+          <span>Destinations</span>
+          <span>Trips</span>
+          <span>About</span>
+          <span>Contact</span>
+        </div>
       </div>
     )
   };
